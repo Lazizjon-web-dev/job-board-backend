8c54b49377eba0eccbc01a896a86108e07f57a33
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-03-10 15:35:12 +0500

    refactor(migrations): Add NOT NULL constraint for salary field in jobs table

--- migrations/20250308034715_create_jobs_table.sql
+++ migrations/20250308034715_create_jobs_table.sql
@@ -4,9 +4,9 @@ CREATE TABLE jobs (
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     location TEXT NOT NULL,
-    salary NUMERIC(10, 2),
+    salary NUMERIC(10, 2) NOT NULL DEFAULT 0.00,
     category TEXT NOT NULL,
-    employer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    employer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
