9831e1b547eaaf065db9c12915ed54b734a123c3
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-03-11 09:22:20 +0500

    feat: Add message field to applications table and default value to `status` column
    - Add message field to application table for storing the content of the application
    - Add default value (`pending`) to status column of the applications table
    
    This commit adds a new `message` field to the `applications` table in the database. The field is of type `TEXT` and is marked as `NOT NULL`. Additionally, a default value of 'pending' is set for the `status` field. This change allows for storing a message along with the application and provides a default status for new applications.

--- migrations/20250308034722_create_applications_table.sql
+++ migrations/20250308034722_create_applications_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE applications (
     id SERIAL PRIMARY KEY,
     job_id INTEGER NOT NULL REFERENCES jobs(id) ON DELETE CASCADE,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'rejected')),
+    message TEXT NOT NULL,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'rejected')) DEFAULT 'pending',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
