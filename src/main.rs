dab7a489817cf692f4784214f86d34dd299f4274
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-03-29 13:48:18 +0500

    feat: Update bind address to listen on all available interfaces
    This commit updates the bind address in the `main.rs` file to listen on all available interfaces (`0.0.0.0`) instead of just `127.0.0.1`. This change allows the application to accept connections from any IP address, making it accessible from the network.

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> std::io::Result<()> {
             .configure(jobs::config)
             .configure(applications::config)
     })
-    .bind("127.0.0.1:8000")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await
 }
