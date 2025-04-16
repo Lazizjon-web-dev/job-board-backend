615582400470e868b19c5af348e94e026fbcedc5
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-16 10:34:50 +0500

    fix: Changed CORS policies, allowed any origin, method and header

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(
                 Cors::default()
-                    .allowed_origin("http://localhost:5173")
-                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_headers(vec!["Content-Type", "Authorization"])
-                    .max_age(3600),
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+                    .supports_credentials()
             )
             .app_data(web::Data::new(pool.clone()))
             .configure(auth::config)
